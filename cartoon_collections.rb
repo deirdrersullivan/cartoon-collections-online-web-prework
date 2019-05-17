bd8176ddbe512d94cc6d6aba68354ff1acb57e57
Deirdre  Sullivan <dsulliv4@binghamton.edu>
2019-05-17 03:58:24 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,8 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each.with_index(1) do (dwarf,index)
 end
+end
+
 
 def summon_captain_planet# code an argument here
   # Your code here
